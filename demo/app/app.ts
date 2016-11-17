9560df3f83b6e42bd9fc6197c7b7cd4d977202fe
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-17 16:58:23 +0000

    removes duplicate polyfill

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,6 +1,3 @@
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
