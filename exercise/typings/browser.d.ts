7a5da840f8b89519904bce0c5a0c52bc4ea00a47
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-17 13:14:29 +0000

    exercise

--- exercise/typings/browser.d.ts
+++ exercise/typings/browser.d.ts
@@ -1,3 +1,2 @@
-/// <reference path="browser/ambient/core-js/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/definitions/zone.js/zone.js.d.ts" />
--- exercise/typings/main.d.ts
+++ exercise/typings/main.d.ts
@@ -1,3 +1,2 @@
-/// <reference path="main/ambient/core-js/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="main/definitions/zone.js/zone.js.d.ts" />
